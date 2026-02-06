2bfb0e495acf523c079192d515cd6637a91cf25c
ceki <ceki@qos.ch>
2026-02-06 19:49:39 +0100

    doc updates, removed Receiver docs, other updates
    Signed-off-by: ceki <ceki@qos.ch>

--- src/site/pages/manual/menu.js
+++ src/site/pages/manual/menu.js
@@ -1,20 +1,20 @@
 
 document.write('<div id="manualIndex">');
 document.write('<p class="menu_header">Chapter Index</p>')
-document.write('<p class="menu"><a href="introduction.html"><b>Ch1: Introduction to logback</b></a></p>');
-document.write('<p class="menu"><a href="architecture.html"><b>Ch2: Architecture</b></a></p>');
-document.write('<p class="menu"><a href="configuration.html"><b>Ch3: Configuration</b></a></p>');
-document.write('<p class="menu"><a href="appenders.html"><b>Ch4: Appenders</b></a></p>');
-document.write('<p class="menu"><a href="encoders.html"><b>Ch5: Encoders</b></a></p>');
-document.write('<p class="menu"><a href="layouts.html"><b>Ch6: Layouts</b></a></p>');
-document.write('<p class="menu"><a href="filters.html"><b>Ch7: Filters</b></a></p>');
-document.write('<p class="menu"><a href="mdc.html"><b>Ch8: Mapped Diagnostic Contexts</b></a></p>');
-document.write('<p class="menu"><a href="loggingSeparation.html"><b>Ch9: Logging Separation</b></a></p>');
-//document.write('<p class="menu"><a href="jmxConfig.html"><b>Ch10: JMX Configurator</b></a></p>');
-document.write('<p class="menu"><a href="onJoran.html"><b>Ch10: Joran</b></a></p>');
-document.write('<p class="menu"><a href="migrationFromLog4j.html"><b>Ch11: Migration from log4j 1.x</b></a></p>');
-document.write('<p class="menu"><a href="receivers.html"><b>Ch12: Receivers</b></a></p>');
-document.write('<p class="menu"><a href="usingSSL.html"><b>Ch13: Using SSL</b></a></p>');
+document.write('<p class="menu"><a href="introduction.html"><b>&bull; Introduction to logback</b></a></p>');
+document.write('<p class="menu"><a href="architecture.html"><b>&bull; Architecture</b></a></p>');
+document.write('<p class="menu"><a href="configuration.html"><b>&bull; Configuration</b></a></p>');
+document.write('<p class="menu"><a href="appenders.html"><b>&bull; Appenders</b></a></p>');
+document.write('<p class="menu"><a href="encoders.html"><b>&bull; Encoders</b></a></p>');
+document.write('<p class="menu"><a href="layouts.html"><b>&bull; Layouts</b></a></p>');
+document.write('<p class="menu"><a href="filters.html"><b>&bull; Filters</b></a></p>');
+document.write('<p class="menu"><a href="mdc.html"><b>&bull; Mapped Diagnostic Contexts</b></a></p>');
+//document.write('<p class="menu"><a href="loggingSeparation.html"><b>Ch9: Logging Separation</b></a></p>');
+//document.write('<p class="menu"><a href="jmxConfig.html"><b>&bull; JMX Configurator</b></a></p>');
+document.write('<p class="menu"><a href="onJoran.html"><b>&bull; Joran</b></a></p>');
+document.write('<p class="menu"><a href="migrationFromLog4j.html"><b>&bull; Migration from log4j 1.x</b></a></p>');
+//document.write('<p class="menu"><a href="receivers.html"><b>Ch12: Receivers</b></a></p>');
+document.write('<p class="menu"><a href="usingSSL.html"><b>&bull; Using SSL</b></a></p>');
 
 document.write('<p style="border: 1px solid #cccccc;"></p>');
 document.write('</div>');
